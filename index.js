b9d0c08dfb68ee352b0f3c7ec71bbd561033e124
OinterO <jonathanjean110@gmail.com>
2020-09-10 23:14:08 -0400

    Jonathan-Jean

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
+
 function createMenuItem(name, cost, category){
-    /* Code here */
+
+
+
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+console.log(createMenuItem('soup', 12, 'Dinner'));
+console.log(createMenuItem('cereal', 2, 'breakfast'));
+console.log(createMenuItem('pizza', 3, 'lunch'));
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -25,6 +31,20 @@ and should return a number.
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
 
+const burger = {
+  name: "Burger",
+  price: 18,
+  category: "Lunch",
+
+  discount: function(discType) {
+    
+    if(discType === "teacher") {
+      return this.price - (this.price * 0.25) + " Is Your New Price"
+    }else if (discType === "student") {
+      return this.price - (this.price * 0.10) + " Is Your New Price" 
+    }
+  } // discount   
+} // burger
 
 ///////////////Reviews (MVP)///////////////////
 
@@ -41,10 +61,21 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 /* Task 3: Console.log just Julius' feedback */
 
 
+function getReviewByIndex(reviews, index) {
+  return `${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review and their feedback was: ${reviews[index].feedback}`
+}
+console.log(getReviewByIndex(reviews, 5));
+
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
+function getReviewByIndex(Jonathan, index) {
+  return `${Jonathan[index].name} gave the restaurant a ${Jonathan[index].rating} star review and their feedback was: ${Jonathan[index].feedback}`
+}
+console.log(getReviewByIndex(reviews, 2));
 
 /* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+
+
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -59,8 +90,8 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
+  return `${reviews[index]}`
+  reviews[0].feedback = "this place is chill with really cool people, great for getting work done on weekdays"  }
   
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
@@ -72,8 +103,8 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(reviews) {
+  return `name} gave the restaurant a {review.rating}, and their feedback was: {feedback}`
   } 
 
 
@@ -124,7 +155,7 @@ Your function should accept:
 
 and return an object.
 
-The returned object should have the following characteristics:
+The returned object should have the followng characteristics:
      it has an `odometer` property that contains the argument passed in.
      it has a `drive` method that takes a distance as its argument, and
          (1) causes the odometer in the object to be increased by the distance,
